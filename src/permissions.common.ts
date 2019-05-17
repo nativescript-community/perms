4610cfa3dc5fbae573e09467e77e5cf7d1f0d091
Martin Guillon <martin.guillon@akylas.fr>
2019-05-17 15:53:57 +0200

    fix: better logs

--- src/permissions.common.ts
+++ src/permissions.common.ts
@@ -1,4 +1,3 @@
-
 let debug = false;
 export function setDebug(value: boolean) {
     debug = value;
@@ -14,12 +13,12 @@ export const CLog = (type: CLogTypes = 0, ...args) => {
     if (debug) {
         if (type === 0) {
             // Info
-            console.log(...args);
+            console.log('[nativescript-perms]', ...args);
         } else if (type === 1) {
             // Warning
-            console.warn(...args);
+            console.warn('[nativescript-perms]', ...args);
         } else if (type === 2) {
-            console.error(...args);
+            console.error('[nativescript-perms]', ...args);
         }
     }
 };
