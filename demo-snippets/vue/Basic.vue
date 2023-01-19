f0a0a3dfb3f7e83d6cb1525e5f8a228543be6efc
Martin Guillon <dev@akylas.fr>
2023-01-19 14:44:56 +0100

    demo: vue demo update

--- demo-snippets/vue/Basic.vue
+++ demo-snippets/vue/Basic.vue
@@ -1,7 +1,14 @@
 <template>
     <Page>
         <ActionBar title="Extendedinfo Demo"> </ActionBar>
-        <ListView ref="listView" :items="permissions" rowHeight="70">
+        <GridLayout rows="auto,*">
+            <WrapLayout> 
+                    <Button text="checkSingle" @tap="checkSinglePerm" />
+                    <Button text="checkMult" @tap="checkMultiplePerms" />
+                    <Button text="reqSingle" @tap="requestSinglePerm" />
+                    <Button text="reqMult" @tap="requestMultiplePerms" />
+            </WrapLayout>
+        <ListView ref="listView" :items="permissions" rowHeight="70" row="1">
             <v-template>
                 <GridLayout columns="*,auto,auto" padding="10" class="item" orientation="horizontal">
                     <Label :text="item" fontSize="17" verticalAlignment="center"></Label>
@@ -10,6 +17,7 @@
                 </GridLayout>
             </v-template>
         </ListView>
+        </GridLayout>
     </Page>
 </template>
 
@@ -75,6 +83,7 @@ export default Vue.extend({
             }
         },
         async requestPermission(perm: Permissions | 'multiple' | 'manage') {
+            console.log('requestPermission', perm)
             try {
                 if (perm === 'manage') {
                     this.askForManagePermission();
@@ -82,19 +91,19 @@ export default Vue.extend({
                     const result = await request({ location: { coarse: false }, storage: { manage: true } });
                     alert(JSON.stringify(result));
                 } else {
-                    if (__ANDROID__ && perm === 'notification') {
-                        // create notification channel
-                        if (sdkVersion >= 26) {
-                            const context = Utils.ad.getApplicationContext();
-                            // API level 26 ("Android O") supports notification channels.
-                            const service = context.getSystemService(android.content.Context.NOTIFICATION_SERVICE) as android.app.NotificationManager;
+                    // if (__ANDROID__ && perm === 'notification') {
+                    //     // create notification channel
+                    //     if (sdkVersion >= 26) {
+                    //         const context = Utils.ad.getApplicationContext();
+                    //         // API level 26 ("Android O") supports notification channels.
+                    //         const service = context.getSystemService(android.content.Context.NOTIFICATION_SERVICE) as android.app.NotificationManager;
 
-                            // create channel
-                            const channel = new android.app.NotificationChannel('test_channel', 'test_channel', android.app.NotificationManager.IMPORTANCE_MIN);
-                            channel.setDescription('test');
-                            service.createNotificationChannel(channel);
-                        }
-                    }
+                    //         // create channel
+                    //         const channel = new android.app.NotificationChannel('test_channel', 'test_channel', android.app.NotificationManager.IMPORTANCE_MIN);
+                    //         channel.setDescription('test');
+                    //         service.createNotificationChannel(channel);
+                    //     }
+                    // }
                     const result = await request(perm, { type: 'always' });
                     alert(JSON.stringify(result));
                 }
@@ -104,33 +113,72 @@ export default Vue.extend({
             }
         },
         async checkManagePermission() {
-            if (__ANDROID__) {
-                return android.os.Build.VERSION.SDK_INT < 30 || android.os.Environment.isExternalStorageManager();
-            }
+            // if (__ANDROID__) {
+            //     return android.os.Build.VERSION.SDK_INT < 30 || android.os.Environment.isExternalStorageManager();
+            // }
             return true;
         },
         async askForManagePermission() {
-            if (__ANDROID__) {
-                const activity = Application.android.startActivity as androidx.appcompat.app.AppCompatActivity;
-                if (this.checkManagePermission()) {
-                    return true;
-                }
-                //If the draw over permission is not available open the settings screen
-                //to grant the permission.
-                return new Promise<boolean>((resolve, reject) => {
-                    const REQUEST_CODE = 6646;
-                    const onActivityResultHandler = (data: AndroidActivityResultEventData) => {
-                        if (data.requestCode === REQUEST_CODE) {
-                            Application.android.off(AndroidApplication.activityResultEvent, onActivityResultHandler);
-                            resolve(android.provider.Settings.canDrawOverlays(activity));
-                        }
-                    };
-                    Application.android.on(AndroidApplication.activityResultEvent, onActivityResultHandler);
-                    const intent = new android.content.Intent(android.provider.Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION, android.net.Uri.parse('package:' + activity.getPackageName()));
-                    activity.startActivityForResult(intent, REQUEST_CODE);
-                });
-            }
+            // if (__ANDROID__) {
+            //     const activity = Application.android.startActivity as androidx.appcompat.app.AppCompatActivity;
+            //     if (this.checkManagePermission()) {
+            //         return true;
+            //     }
+            //     //If the draw over permission is not available open the settings screen
+            //     //to grant the permission.
+            //     return new Promise<boolean>((resolve, reject) => {
+            //         const REQUEST_CODE = 6646;
+            //         const onActivityResultHandler = (data: AndroidActivityResultEventData) => {
+            //             if (data.requestCode === REQUEST_CODE) {
+            //                 Application.android.off(AndroidApplication.activityResultEvent, onActivityResultHandler);
+            //                 resolve(android.provider.Settings.canDrawOverlays(activity));
+            //             }
+            //         };
+            //         Application.android.on(AndroidApplication.activityResultEvent, onActivityResultHandler);
+            //         const intent = new android.content.Intent(android.provider.Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION, android.net.Uri.parse('package:' + activity.getPackageName()));
+            //         activity.startActivityForResult(intent, REQUEST_CODE);
+            //     });
+            // }
             return true;
+        },
+
+        checkSinglePerm() {
+            check('location', { type: 'always' })
+                .then((response) => {
+                    console.log('[TCL] ~ file: Home.vue:50 ~ .then ~ response', response)
+                });
+        },
+        checkMultiplePerms() {
+            const permissions = {
+                location: {},
+                contacts: {},
+            };
+            checkMultiple(permissions)
+                .then((response) => {
+                    console.log('[TCL] ~ file: Home.vue:60 ~ .then ~ response', response)
+                })
+                .catch((error) => {
+                    console.log('[TCL] ~ file: Home.vue:63 ~ .catch ~ error', error)
+                });
+        },
+        requestSinglePerm() {
+            request('location', { type: 'always' })
+                .then((response) => {
+                    console.log('[TCL] ~ file: Home.vue:66 ~ .then ~ response', response)
+                });
+        },
+        requestMultiplePerms() {
+            const permissions = {
+                location: { type: 'always' },
+                contacts: {},
+            };
+            request(permissions)
+                .then((response) => {
+                    console.log('[TCL] ~ file: Home.vue:76 ~ .then ~ response', response)
+                })
+                .catch((error) => {
+                    console.log('[TCL] ~ file: Home.vue:63 ~ .catch ~ error', error)
+                });
         }
     }
 });
